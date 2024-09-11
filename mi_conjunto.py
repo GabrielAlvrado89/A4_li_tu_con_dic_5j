3789bb266e33c72afbdf223918508dd28d684ec4
Gabriel Alvarado <a.22308051281127@cbtis128.edu.mx>
2024-09-11 07:38:54 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,57 @@
+print("Ejemplo conjuntos set")
+Tipodedeportes = {"futbol", "Tenis", "Basquetbol"}
+print(Tipodedeportes)
+print("Lista elementos del conjunto tipodedeportes loop for")
+for Alvarado in Tipodedeportes:
+    print(Alvarado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
