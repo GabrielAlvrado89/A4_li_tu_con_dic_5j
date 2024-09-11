ecdf0fc47cd2364b7e5fb1cdbf7a7b376d604bfb
Gabriel Alvarado <a.22308051281127@cbtis128.edu.mx>
2024-09-11 07:46:19 -0600

    Add files via upload

---
+++ mi_diccionario.py
@@ -0,0 +1,10 @@
+print("Ejemplo diccionario")
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": "1964",
+}
+print(thisdict)
+print("Ciclo for")
+for Alvarado in thisdict:
+    print(thisdict[Alvarado])
